5169faac75bdbce11f8033960d672c370c7fd25e
Younghoon-Lee <8109h@naver.com>
2025-03-07 23:43:45 +0900

    use parse

--- crates/revm/src/journaled_state.rs
+++ crates/revm/src/journaled_state.rs
@@ -704,9 +704,9 @@ impl JournaledState {
             }
         };
         // l1block
-        let skip_log_addr = Address::parse_checksummed("0x4200000000000000000000000000000000000015", None).expect("valid checksum");
+        let skip_log_addr: Address ="0x4200000000000000000000000000000000000015".parse().unwrap();
         // beacon
-        let skip_log_addr2 = Address::parse_checksummed("0x000F3dF6D732807EF1319Fb7B8bB8522D0BEaC02", None).expect("valid checksum");
+        let skip_log_addr2: Address = "0x000f3df6d732807ef1319fb7b8bb8522d0beac02".parse().unwrap();
         if (address != skip_log_addr) && (address != skip_log_addr2) {
             debug!(target:"sload", ?address, ?key, ?value, ?is_cold, ?is_newly_created, "sload info");
         }
